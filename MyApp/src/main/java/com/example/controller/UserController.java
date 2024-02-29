01c8b66b76f7e27f43eae658b56ce747d0737d3d
N A V E E N <N A V E E N@DESKTOP-5C1B8QA>
2024-02-29 17:19:54 +0530

    request body added

--- MyApp/src/main/java/com/example/controller/UserController.java
+++ MyApp/src/main/java/com/example/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class UserController {
 	private UserDao dao;
 	
 	@PostMapping(path="/addUser")
-	public User createUser(User user)
+	public User createUser(@RequestBody User user)
 	{
 		return dao.addUser(user);
 	}
